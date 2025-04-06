b5629040662a81e94319431ef4c5767958008cc9
Guzman Canivares Enzo Nicolas <nicocanivares2002@gmail.com>
2025-04-06 15:24:13 -0300

    Funcion Listar PCs

--- tp2_4.c
+++ tp2_4.c
@@ -10,18 +10,64 @@ struct {
     char *tipo_cpu; // Tipo de procesador (apuntador a cadena de caracteres)
     } typedef compu;
 
+    void listarPCs(compu pc[], int cantidad);
+
 int main()
 {
     srand(time(NULL));
     char tipos[6][10] = {"Intel", "AMD", "Celeron", "Athlon", "Core", "Pentium"};
     int velocidad, anio, cantNucleos, tipo;
+    char *punt = &tipos[0][0];
     compu compus[5];
-    //velocidad
-    velocidad = 1 + rand() % 3;
-    anio = 2015 + rand() % 2024;
-    cantNucleos = 1 + rand() % 8;
-    tipo = rand() % 5;
 
+    //cargar Struct
+    for (int i = 0; i < 5; i++)
+    {
+        //velocidad aleatorio
+        velocidad = 1 + rand() % 3;
+        //anio aleatorio    
+        anio = 2015  + rand() % (2025 - 2015 + 1);
+        //cantNucleos aleatorios
+        cantNucleos = 1 + rand() % 8;
+        //tipo cpu aleatorio
+        tipo = rand() % 5;
+        compus[i].velocidad = velocidad;
+        compus[i].anio =  anio;
+        compus[i].cantidad_nucleos = cantNucleos;
+        compus[i].tipo_cpu = tipos[tipo];
+        /* for (int j = 0; j < 10; j++)
+        {
+            compus[i].tipo_cpu = *(punt + (tipo * 10 + j));
+        } */
+    }
+
+    listarPCs(compus, 5);
+
+    
+/*     for (int i = 0; i < 5; i++)
+    {
+        printf("\nVelocidad: %dGhz", compus[i].velocidad);
+        printf("\nAnio de salida: %d", compus[i].anio);
+        printf("\nCantidad de nucleos: %d", compus[i].cantidad_nucleos);
+        printf("\nTipo de cpu: ");
+        puts(compus[i].tipo_cpu);
+    }
+     */
 
     return 0;
+}
+
+
+void listarPCs(compu pc[], int cantidad){
+
+    for (int i = 0; i < cantidad; i++)
+    {
+        printf("\n---- PC numero %d ----", i+1);
+        printf("\nVelocidad: %dGhz", pc[i].velocidad);
+        printf("\nAnio de salida: %d", pc[i].anio);
+        printf("\nCantidad de nucleos: %d", pc[i].cantidad_nucleos);
+        printf("\nTipo de cpu: ");
+        puts(pc[i].tipo_cpu);
+        printf("------------------------\n");
+    }
 }
